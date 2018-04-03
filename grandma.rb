84bd04bf244f05b069499bfe67f6a4fa688276f0
Mary Jack <maryjackinthebox@gmail.com>
2018-04-03 22:52:09 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -17,7 +17,7 @@ def speak_to_grandma (speaks)
     puts "HUH?! SPEAK UP, SONNY"
   elsif speaks == speaks.upcase
     puts "NO, NOT SINCE 1938!"
-  else phrase == "I LOVE YOU GRANDMA!"
+  else speaks == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
   end#ladder
   
