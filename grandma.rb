74f939b48e8da6264452b37bc6851aaf71986f71
Mary Jack <maryjackinthebox@gmail.com>
2018-04-03 22:38:51 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -12,7 +12,7 @@
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
 
-def speak_to_grandma (speaks)
+def speak_to_grandma (speaks, "I LOVE YOU GRANDMA!")
   if 
     puts "HUH?! SPEAK UP, SONNY"
   elsif
